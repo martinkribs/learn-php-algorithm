064036da0bdeca882207169ca0716f959c2dfc2b
Martin Kribs <54333035+martinkribs@users.noreply.github.com>
2022-01-23 19:27:19 +0100

    new algorithm

--- src/003-lengthOfLongestSubstring.php
+++ src/003-lengthOfLongestSubstring.php
@@ -2,9 +2,27 @@
 
 namespace learn\src;
 
+$tests=[
+    "abcabcbb" => 3,
+    "bbbbb" => 1,
+    "pwwkew" => 3,
+    "" => 0,
+    " " => 1,
+    "au" => 2,
+    "dvdf" => 3,
+    "abba" => 2
+];
+
 $hello = new Solution;
-$solution = $hello->lengthOfLongestSubstring("");
-print_r($solution);
+foreach ($tests as $test => $value) {
+    $solution = $hello->lengthOfLongestSubstring($test);
+    if ($solution==$value){
+        print_r("Test '". $test. "' erfolgreich! \n");
+    } else {
+        print_r("[ERROR] Test '". $test. "' fehlgeschlagen \n");
+    }
+    Print("\n");
+}
 
 class Solution {
 
@@ -13,36 +31,50 @@ class Solution {
      * @return Integer
      */
     function lengthOfLongestSubstring($s) {
-        $result=null;
-        $counter=0;
-        if(empty($s)){
-            return 0;
-        }
-        $charArray = str_split($s);
 
-        for ($i=0;$i<count($charArray);$i++) {
+        // Length of the given input string
+        $n = strlen($s);
 
-            $double = array_search($charArray[$i], array_slice($charArray, $i+1));
-            if(!$double===false){
+        //string to array
+        $arr = str_split($s);
 
-                $resultArray = array_slice($charArray, $i,$double+1);
-                if (count($resultArray)>$counter){
-                    $counter = count($resultArray);
-                }
+        // Length of longest substring
+        $result=0;
 
-            } elseif (false==is_bool($double)) {
-                $resultArray = array_slice($charArray, $i,$double+1);
-                if (count($resultArray)>$counter){
-                    $counter = count($resultArray);
-                }
-            } else {
-                $resultArray = array_slice($charArray, $i,$double+1);
-                if (count($resultArray)>$counter){
-                    $counter = count($resultArray);
-                }
+        // Map to store visited characters along with their index
+        $charIndexMap=array();
+
+        // start indicates the start of current substring
+        $start=0;
+
+        // Iterate through the string and slide the window each time you find a duplicate
+        // end indicates the end of current substring
+        for ($end = 0; $end < $n; $end++ ){
+
+            // Check if the current character is a duplicate
+            $isDuplicate = array_key_exists($arr[$end],$charIndexMap);
+            if ($isDuplicate) {
+                $duplicateIndex = $charIndexMap[$arr[$end]];
+                // Update the result for the substring in the current window before we found duplicate character
+                $result = max($result, $end-$start);
+
+                // Remove all characters before the new
+                for ($i = $start; $i <= $duplicateIndex; $i++) {
+                    $key = $s[$i];
+                    unset($charIndexMap[$key]);
+                } 
+
+                // Slide the window since we have found a duplicate character
+                $start = $duplicateIndex + 1;
             }
+            // Add the current character to hashmap
+            $charIndexMap[$arr[$end]] = $end;
         }
-        return $counter;
+        // Update the result for last window
+        // For a input like "abc" the execution will not enter the above if statement for checking duplicates
+        $result = max($result, $n-$start);
+
+        return $result;
     }
 }
 ?>
